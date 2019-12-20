0c7ab54366edf6cf312b41a708c8def9529a9f08
Harry  Barber <harrybarber@protonmail.com>
2019-12-20 16:41:01 +0700

    Improve example

--- examples/basic.rs
+++ examples/basic.rs
@@ -3,12 +3,8 @@ use tracing_gelf::TcpLogger;
 #[tokio::main]
 async fn main() {
     // Init tracing
-    let (logger, bg_task) = TcpLogger::builder()
-        .connect("10.1.1.221:12201", 300)
-        .unwrap();
+    let bg_task = TcpLogger::builder().init("10.1.1.221:12201").unwrap();
     tokio::spawn(bg_task);
-    tracing_core::dispatcher::set_global_default(tracing_core::dispatcher::Dispatch::new(logger))
-        .expect("happen, happening, happened");
 
     let span = tracing::info_span!("my span");
     span.in_scope(|| {
