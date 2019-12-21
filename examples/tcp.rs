278f91272eacc2cee3d90535c0dff2c9d83a79f5
Harry Barber <harrybarber@protonmail.com>
2019-12-21 13:46:11 +0700

    Improve example

---
+++ examples/tcp.rs
@@ -0,0 +1,27 @@
+use tracing_gelf::Logger;
+
+#[tokio::main]
+async fn main() {
+    // Graylog address
+    let address = "10.1.1.221:12201";
+
+    // Start tracing
+    let bg_task = Logger::builder().init_tcp(address).unwrap();
+
+    // Spawn background task
+    // Any futures executor can be used
+    tokio::spawn(bg_task);
+
+    // Send a log to Graylog
+    tracing::info!(message = "oooh what's in here?");
+
+    // Create a span
+    let span = tracing::info_span!("cave");
+    span.in_scope(|| {
+        // Log inside a span
+        tracing::info!(message = "oh god, it's dark in here");
+    });
+
+    // Log a structured log
+    tracing::info!(message = "i'm glad to be out", spook_lvl = 3, ruck_sack = ?["glasses", "inhaler", "large bat"]);
+}
